4d21764357db4c424fb6928c371f499c5b8aa43f
ElijahAranzamendez <mashedeli456@gmail.com>
2026-06-05 10:16:08 +0800

    submissions/Carlo-sql-exercises

--- submissions/submissions-carlo/it_carlo_exercise02.sql
+++ submissions/submissions-carlo/it_carlo_exercise02.sql
@@ -8,14 +8,14 @@
 -- Exercise 8 (PARAMETERIZED): Write a parameterized query for employee lookup by email.
 
 -- For testing
--- SELECT * 
--- FROM employees
+SELECT * 
+FROM employees
 
--- SELECT * 
--- FROM employee_transactions
+SELECT * 
+FROM employee_transactions
 
--- SELECT *
--- FROM report_runs
+SELECT *
+FROM report_runs
 
 -- Exercise 1
 INSERT INTO employees
@@ -59,8 +59,10 @@ ORDER BY
 --Refer to views/01_reporting_views.sql
 SELECT *
 FROM vw_department_headcount
---The view is the same as every instantiated sql lines. 
---In my own understanding, this is like a reusable class that you can call to perform specific tasks
+--This Query pulls data from the departments table joined with the employee table
+--The first 3 columns pulls the data as is. For the active_employee_count column, it has a condition where it COUNTS ONLY the active emplyoees in the table (has a true condition)
+--The last column pulls the same data table and COUNTS all the employee_id
+--In my own understanding, this is like a reusable class that you can call to perform specific tasks when needed.
 
 
 --Exercise 7
@@ -70,6 +72,9 @@ FROM fn_department_headcount AS headcount
 
 SELECT *
 FROM fn_employee_transaction_total(2)
+--This function pulls all the sum of the employee_transactions of the employee_id
+--The COALESCE is imporant here as the process includes all of the employee_id even if it has no employee_transactions
+--same with the previous exercise, this is a class function that can be called.
 
 
 --Exercise 8
@@ -83,4 +88,8 @@ WHERE email = $1;
 
 EXECUTE vlookupMail('ava.stone@example.com');
 DEALLOCATE vlookupMail;
+--For JAVA you can also instantiate the SQL string by doing this
+--String vlookupMail = "SELECT employee_number,first_name,last_name FROM employees WHERE email = ?";
+
+
 
